1b1615f81c64a9e7588ddf256ffa401a9ba7f799
David Bankole Abel <ballack_davidabel@yahoo.com>
2022-07-25 20:48:39 -0400

    Loading second activity

--- app/src/main/java/com/db/contacttracingapp/MainActivity.java
+++ app/src/main/java/com/db/contacttracingapp/MainActivity.java
@@ -3,12 +3,33 @@ package com.db.contacttracingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity<button> extends AppCompatActivity {
+        Button button1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+
+
+        button1 = findViewById(R.id.button_one);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(MainActivity.this,MainActivityInfo.class);
+                startActivity(intent);
+            }
+        });
+
+        }
 }
