a84cbe6206855fb1bc12548242b6ec90ce72da9b
David Bankole Abel <ballack_davidabel@yahoo.com>
2022-07-30 22:32:54 -0400

    Get user input

--- app/src/main/java/com/db/contacttracingapp/MainActivity.java
+++ app/src/main/java/com/db/contacttracingapp/MainActivity.java
@@ -3,13 +3,23 @@ package com.db.contacttracingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity<button> extends AppCompatActivity {
         Button button1;
+        ListView listView;
+        ArrayList<String> list;
+        ArrayAdapter<String> arrayAdapter;
+
 
 
 
@@ -18,9 +28,12 @@ public class MainActivity<button> extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        listView = (ListView) findViewById(R.id.listviewy);
         button1 = findViewById(R.id.button_one);
+
+        list = new ArrayList<String>();
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),
+        android.R.layout.simple_list_item_1,list);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,7 +41,11 @@ public class MainActivity<button> extends AppCompatActivity {
 
                 Intent intent=new Intent(MainActivity.this,MainActivityInfo.class);
                 startActivity(intent);
+                
+                listView.setAdapter(arrayAdapter);
+                arrayAdapter.notifyDataSetChanged();
             }
+
         });
 
         }
--- app/src/main/java/com/db/contacttracingapp/MainActivityInfo.java
+++ app/src/main/java/com/db/contacttracingapp/MainActivityInfo.java
@@ -2,13 +2,34 @@ package com.db.contacttracingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivityInfo extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+    String st;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_info);
+
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText)findViewById(R.id.editTextTextPersonName2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Name=editText.getText().toString();
+
+            }
+        });
+
+        };
+
     }
-}
